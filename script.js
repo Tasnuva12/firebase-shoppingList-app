95394c46926a85cd3c849cec78aa7ddc010bad73
Tasnuva12 <tasnuvamehnaz@iut-dhaka.edu>
2024-05-01 19:45:05 +0600

    Building the UI
     css for the image , input field and the button

---
+++ script.js
@@ -0,0 +1,7 @@
+const inputEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+
+addButtonEl.addEventListener("click",function(){
+    let inputValue = inputEl.value;
+    console.log(inputValue);
+})
